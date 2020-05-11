f5c5e5ce9a999462306998f6621828a58a70bf48
Hesam Aldin Ahani <hesamahani@gmail.com>
2020-05-11 12:27:00 +0430

    tapsell native ads -> custom

---
+++ adhub/src/main/java/com/fara/projects/adhub/utils/RatioImageView.java
@@ -0,0 +1,18 @@
+package com.fara.projects.adhub.utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class RatioImageView extends ir.tapsell.sdk.nativeads.views.RatioImageView {
+    public RatioImageView(Context context) {
+        super(context);
+    }
+
+    public RatioImageView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+    }
+
+    public RatioImageView(Context context, AttributeSet attributeSet, int i) {
+        super(context, attributeSet, i);
+    }
+}
